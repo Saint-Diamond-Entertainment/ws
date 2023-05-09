c8af0a5f5e4b0131891773498d9908660fee1ffc
Dmitry MUZYCHENKA <dzmitry.muzychenka@cherishdev.com>
2023-05-09 13:54:50 +0300

    refactor: add WS class

--- dist/index.js
+++ dist/index.js
@@ -12,127 +12,132 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.initialize = exports.create = exports.clients = exports.wss = void 0;
 const ws_1 = require("ws");
 const uuid_1 = require("uuid");
 const fs_1 = require("fs");
 const http_1 = __importDefault(require("http"));
 const https_1 = __importDefault(require("https"));
-const room_1 = require("./room");
 const websocket_1 = require("./constants/websocket");
-let server;
-exports.clients = new Map();
-let pingTimer;
-let pingStep = websocket_1.DEFAULT_INTERVAL;
-let logErrors = websocket_1.DEFAULT_ERRORS_LOGGING;
-function create(data) {
-    const { cert, debug, key, port, secured } = data;
-    const ip = data.ip || websocket_1.DEFAULT_IP;
-    if (typeof data.pingStep === 'number') {
-        pingStep = data.pingStep;
-    }
-    if (typeof data.logErrors === 'boolean') {
-        logErrors = data.logErrors;
-    }
-    if (secured) {
-        if (!cert || !key) {
-            throw new Error('No cert/key definition');
+class WS {
+    constructor(data) {
+        this.clients = new Map();
+        this.rooms = new Map();
+        this.pingStep = websocket_1.DEFAULT_INTERVAL;
+        this.logErrors = websocket_1.DEFAULT_ERRORS_LOGGING;
+        const { cert, debug, key, port, secured } = data;
+        const ip = data.ip || websocket_1.DEFAULT_IP;
+        if (typeof data.pingStep === 'number') {
+            this.pingStep = data.pingStep;
         }
-        server = https_1.default.createServer({
-            cert: (0, fs_1.readFileSync)(cert),
-            key: (0, fs_1.readFileSync)(key)
-        });
-    }
-    else {
-        server = http_1.default.createServer();
-    }
-    exports.wss = new ws_1.WebSocketServer({ server });
-    initialize();
-    server.listen(port, ip, () => {
-        debug && console.log('✅ WebSocket server is listening on ' + ip + ':' + port);
-    });
-}
-exports.create = create;
-pingTimer = setInterval(() => {
-    exports.clients
-        .forEach(client => {
-        if (!client.isAlive) {
-            return client.disconnect();
+        if (typeof data.logErrors === 'boolean') {
+            this.logErrors = data.logErrors;
         }
-        client.isAlive = false;
-        client.ping();
-    });
-}, pingStep);
-function initialize() {
-    exports.wss.on('connection', (client) => __awaiter(this, void 0, void 0, function* () {
-        client.isAlive = true;
-        client.id = (0, uuid_1.v4)();
-        client.disconnect = () => {
-            try {
-                client.terminate();
-                process.nextTick(() => {
-                    room_1.rooms
-                        .forEach(room => {
-                        room.clients.delete(client.id);
-                    });
-                    exports.clients.delete(client.id);
-                });
+        if (secured) {
+            if (!cert || !key) {
+                throw new Error('No cert/key definition');
             }
-            catch (e) {
-                logErrors && console.error('Error while disconnecting: ', e);
-            }
-        };
-        client.join = (room) => {
-            var _a;
-            (_a = room_1.rooms.get(room)) === null || _a === void 0 ? void 0 : _a.clients.add(client.id);
-        };
-        client.leave = (room) => {
-            var _a;
-            (_a = room_1.rooms.get(room)) === null || _a === void 0 ? void 0 : _a.clients.delete(client.id);
-        };
-        client.broadcast = (room, data, loopback = false) => {
-            var _a;
-            (_a = room_1.rooms.get(room)) === null || _a === void 0 ? void 0 : _a.clients.forEach(broadcastClientId => {
-                var _a;
-                if (!loopback && broadcastClientId === client.id) {
-                    return;
-                }
-                (_a = exports.clients.get(broadcastClientId)) === null || _a === void 0 ? void 0 : _a.send(JSON.stringify(data));
+            this._server = https_1.default.createServer({
+                cert: (0, fs_1.readFileSync)(cert),
+                key: (0, fs_1.readFileSync)(key)
             });
-        };
-        client.call = (data) => {
-            try {
-                client.send(JSON.stringify(data));
-            }
-            catch (e) {
-                logErrors && console.error('Error while parsing data: ', e);
-            }
-        };
-        client.on('message', message => {
-            try {
-                const normalizedMessage = JSON.parse(message.toString());
-                if (!normalizedMessage.type) {
-                    throw new Error('No message type');
+        }
+        else {
+            this._server = http_1.default.createServer();
+        }
+        this.wss = new ws_1.WebSocketServer({ server: this._server });
+        this.initialize();
+        this.pingInterval = setInterval(() => {
+            console.log('clients ', this.clients.size);
+            this.clients
+                .forEach(client => {
+                if (!client.isAlive) {
+                    return client.disconnect();
                 }
-                const type = normalizedMessage.type;
-                const data = normalizedMessage.data || {};
-                client.emit(type, data);
-            }
-            catch (e) {
-                logErrors && console.error('Error while parsing message: ', e);
-            }
+                client.isAlive = false;
+                client.ping();
+            });
+        }, this.pingStep);
+        this._server.listen(port, ip, () => {
+            debug && console.log('✅ WebSocket server is listening on ' + ip + ':' + port);
         });
-        client.on('pong', () => {
+    }
+    initialize() {
+        this.wss.on('connection', (client) => __awaiter(this, void 0, void 0, function* () {
             client.isAlive = true;
+            client.id = (0, uuid_1.v4)();
+            client.disconnect = () => {
+                try {
+                    client.terminate();
+                    process.nextTick(() => {
+                        this.rooms
+                            .forEach(room => {
+                            room.clients.delete(client.id);
+                        });
+                        this.clients.delete(client.id);
+                    });
+                }
+                catch (e) {
+                    this.logErrors && console.error('Error while disconnecting: ', e);
+                }
+            };
+            client.join = (room) => {
+                var _a;
+                (_a = this.rooms.get(room)) === null || _a === void 0 ? void 0 : _a.clients.add(client.id);
+            };
+            client.leave = (room) => {
+                var _a;
+                (_a = this.rooms.get(room)) === null || _a === void 0 ? void 0 : _a.clients.delete(client.id);
+            };
+            client.broadcast = (room, data, loopback = false) => {
+                var _a;
+                (_a = this.rooms.get(room)) === null || _a === void 0 ? void 0 : _a.clients.forEach(broadcastClientId => {
+                    var _a;
+                    if (!loopback && broadcastClientId === client.id) {
+                        return;
+                    }
+                    (_a = this.clients.get(broadcastClientId)) === null || _a === void 0 ? void 0 : _a.send(JSON.stringify(data));
+                });
+            };
+            client.call = (data) => {
+                try {
+                    client.send(JSON.stringify(data));
+                }
+                catch (e) {
+                    this.logErrors && console.error('Error while parsing data: ', e);
+                }
+            };
+            client.on('message', message => {
+                try {
+                    const normalizedMessage = JSON.parse(message.toString());
+                    if (!normalizedMessage.type) {
+                        throw new Error('No message type');
+                    }
+                    const type = normalizedMessage.type;
+                    const data = normalizedMessage.data || {};
+                    client.emit(type, data);
+                }
+                catch (e) {
+                    this.logErrors && console.error('Error while parsing message: ', e);
+                }
+            });
+            client.on('pong', () => {
+                client.isAlive = true;
+            });
+            client.on('close', () => {
+                client.disconnect();
+            });
+            this.wss.clients.clear();
+            this.clients.set(client.id, client);
+        }));
+        this.wss.on('close', () => {
+            clearInterval(this.pingInterval);
         });
-        client.on('close', () => {
-            client.disconnect();
-        });
-        exports.wss.clients.clear();
-        exports.clients.set(client.id, client);
-    }));
-    exports.wss.on('close', () => {
-        clearInterval(pingTimer);
-    });
+    }
+    createRoom(name) {
+        this.rooms.set(name, { clients: new Set() });
+    }
+    deleteRoom(name) {
+        this.rooms.delete(name);
+    }
 }
-exports.initialize = initialize;
+exports.default = WS;
--- src/index.ts
+++ src/index.ts
@@ -3,149 +3,161 @@ import { v4 as uuidv4 } from 'uuid'
 import { readFileSync } from 'fs'
 import http from 'http'
 import https from 'https'
-import { rooms } from './room'
+import { IRoom } from './interfaces/room'
 import { IServerData } from './interfaces/websocket'
 import { Server } from './types/websocket'
 import { DEFAULT_ERRORS_LOGGING, DEFAULT_INTERVAL, DEFAULT_IP } from './constants/websocket'
 
-let server: Server
+export default class WS {
+    private _server: Server
 
-export let wss: WebSocket.Server<WebSocket.WebSocket>
+    wss: WebSocket.Server<WebSocket.WebSocket>
 
-export const clients: Map<string, WebSocket.WebSocket> = new Map()
+    clients: Map<string, WebSocket.WebSocket> = new Map()
+    rooms: Map<string, IRoom> = new Map()
 
-let pingTimer: NodeJS.Timer
-let pingStep: number = DEFAULT_INTERVAL
-let logErrors: boolean = DEFAULT_ERRORS_LOGGING
+    pingInterval: NodeJS.Timer
+    pingStep: number = DEFAULT_INTERVAL
+    logErrors: boolean = DEFAULT_ERRORS_LOGGING
+    
+    constructor (data: IServerData) {
+        const { cert, debug, key, port, secured } = data
+        const ip: string = data.ip || DEFAULT_IP
 
-export function create (data: IServerData) {
-    const { cert, debug, key, port, secured } = data
-    const ip: string = data.ip || DEFAULT_IP
-
-    if (typeof data.pingStep === 'number') {
-        pingStep = data.pingStep
-    }
-
-    if (typeof data.logErrors === 'boolean') {
-        logErrors = data.logErrors
-    }
-
-    if (secured) {
-        if (!cert || !key) {
-            throw new Error('No cert/key definition')
+        if (typeof data.pingStep === 'number') {
+            this.pingStep = data.pingStep
         }
 
-        server = https.createServer({ 
-            cert: readFileSync(cert),
-            key: readFileSync(key)
-        })
-    }
-    else {
-        server = http.createServer()
-    }
-
-    wss = new WebSocketServer({ server })
-
-    initialize()
-
-    server.listen(port, ip, () => {
-        debug && console.log('✅ WebSocket server is listening on ' + ip + ':' + port)
-    })
-}
-
-pingTimer = setInterval(() => {
-    clients
-        .forEach(client => {
-            if (!client.isAlive) {
-                return client.disconnect()
-            }
-            
-            client.isAlive = false
-            client.ping()
-        })
-}, pingStep)
-
-export function initialize () {
-    wss.on('connection', async client => {
-        client.isAlive = true
-        client.id = uuidv4()
-
-        client.disconnect = () => {
-            try {
-                client.terminate()
-                process.nextTick(() => {
-                    rooms
-                        .forEach(room => {
-                            room.clients.delete(client.id)
-                        })
-                    clients.delete(client.id)
-                })
-            }
-            catch (e) {
-                logErrors && console.error('Error while disconnecting: ', e)
-            }
+        if (typeof data.logErrors === 'boolean') {
+            this.logErrors = data.logErrors
         }
 
-        client.join = (room: string) => {
-            rooms.get(room)?.clients.add(client.id)
+        if (secured) {
+            if (!cert || !key) {
+                throw new Error('No cert/key definition')
+            }
+
+            this._server = https.createServer({ 
+                cert: readFileSync(cert),
+                key: readFileSync(key)
+            })
         }
+        else {
+            this._server = http.createServer()
+        }
+
+        this.wss = new WebSocketServer({ server: this._server })
+
+        this.initialize()
         
-        client.leave = (room: string) => {
-            rooms.get(room)?.clients.delete(client.id)
-        }
-
-        client.broadcast = (room: string, data: JSON, loopback: boolean = false) => {
-            rooms.get(room)
-                ?.clients
-                .forEach(broadcastClientId => {
-                    if (!loopback && broadcastClientId === client.id) {
-                        return
+        this.pingInterval = setInterval(() => {
+            console.log('clients ', this.clients.size)
+            this.clients
+                .forEach(client => {
+                    if (!client.isAlive) {
+                        return client.disconnect()
                     }
                     
-                    clients.get(broadcastClientId)?.send(JSON.stringify(data))
+                    client.isAlive = false
+                    client.ping()
                 })
-        }
+        }, this.pingStep)
 
-        client.call = (data: JSON) => {
-            try {
-                client.send(JSON.stringify(data))
-            }
-            catch (e) {
-                logErrors && console.error('Error while parsing data: ', e)
-            }
-        }
-        
-        client.on('message', message => {
-            try {
-                const normalizedMessage = JSON.parse(message.toString())
-                
-                if (!normalizedMessage.type) {
-                    throw new Error('No message type')
-                }
-                
-                const type: string = normalizedMessage.type
-                const data: object = normalizedMessage.data || {}
-                
-                client.emit(type, data)
-            }
-            catch (e) {
-                logErrors && console.error('Error while parsing message: ', e)
-            }
+        this._server.listen(port, ip, () => {
+            debug && console.log('✅ WebSocket server is listening on ' + ip + ':' + port)
         })
-
-        client.on('pong', () => {
+    }
+    
+    initialize () {
+        this.wss.on('connection', async client => {
             client.isAlive = true
+            client.id = uuidv4()
+    
+            client.disconnect = () => {
+                try {
+                    client.terminate()
+                    process.nextTick(() => {
+                        this.rooms
+                            .forEach(room => {
+                                room.clients.delete(client.id)
+                            })
+                            this.clients.delete(client.id)
+                    })
+                }
+                catch (e) {
+                    this.logErrors && console.error('Error while disconnecting: ', e)
+                }
+            }
+    
+            client.join = (room: string) => {
+                this.rooms.get(room)?.clients.add(client.id)
+            }
+            
+            client.leave = (room: string) => {
+                this.rooms.get(room)?.clients.delete(client.id)
+            }
+    
+            client.broadcast = (room: string, data: JSON, loopback: boolean = false) => {
+                this.rooms.get(room)
+                    ?.clients
+                    .forEach(broadcastClientId => {
+                        if (!loopback && broadcastClientId === client.id) {
+                            return
+                        }
+                        
+                        this.clients.get(broadcastClientId)?.send(JSON.stringify(data))
+                    })
+            }
+    
+            client.call = (data: JSON) => {
+                try {
+                    client.send(JSON.stringify(data))
+                }
+                catch (e) {
+                    this.logErrors && console.error('Error while parsing data: ', e)
+                }
+            }
+            
+            client.on('message', message => {
+                try {
+                    const normalizedMessage = JSON.parse(message.toString())
+                    
+                    if (!normalizedMessage.type) {
+                        throw new Error('No message type')
+                    }
+                    
+                    const type: string = normalizedMessage.type
+                    const data: object = normalizedMessage.data || {}
+                    
+                    client.emit(type, data)
+                }
+                catch (e) {
+                    this.logErrors && console.error('Error while parsing message: ', e)
+                }
+            })
+    
+            client.on('pong', () => {
+                client.isAlive = true
+            })
+    
+            client.on('close', () => {
+                client.disconnect()
+            })
+            
+            this.wss.clients.clear()
+            this.clients.set(client.id, client)
         })
-
-        client.on('close', () => {
-            client.disconnect()
+    
+        this.wss.on('close', () => {
+            clearInterval(this.pingInterval)
         })
-        
-        wss.clients.clear()
-        clients.set(client.id, client)
-    })
-
-    wss.on('close', () => {
-        clearInterval(pingTimer)
-    })
+    }
+    
+    createRoom (name: string) {
+        this.rooms.set(name, { clients: new Set() })
+    }
+    
+    deleteRoom (name: string) {
+        this.rooms.delete(name)
+    }
 }
