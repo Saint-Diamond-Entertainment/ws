1225468edf8e0c245ac7d0203ce115fcf77077e9
musicchenko <nogrevonlight@gmail.com>
2024-05-09 22:05:20 +0300

    feat: move declare to websocket types

--- packages/ws/src/types/websocket.ts
+++ packages/ws/src/types/websocket.ts
@@ -1,5 +1,6 @@
 import http from 'http'
 import https from 'https'
+import ws from 'ws'
 import type { IAccount } from './account'
 
 export type TServer = https.Server | http.Server | undefined
@@ -28,4 +29,17 @@ export interface IServerData {
     pingInterval?: number
     port?: number
     secured?: boolean
+}
+
+declare module 'ws' {
+    export interface WebSocket extends ws {
+        account: IAccount
+        broadcast: Function
+        call: Function
+        disconnect: Function
+        id: string
+        isAlive: boolean
+        join: Function
+        leave: Function
+    }
 }
